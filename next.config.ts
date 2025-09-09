818c1fec28b86aaa5b757c678a1b07f6650582a1
qzali-dev <qzali144@gmail.com>
2025-09-08 22:43:57 -0400

    removed base path to test static paths

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  reactStrictMode: true,
-  basePath: "/portfolio-site"
+  reactStrictMode: true
 };
 
 export default nextConfig;

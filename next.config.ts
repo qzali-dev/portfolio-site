2f2b53eca875270ec7d5e62ddea634b26bf75bd8
qzali-dev <qzali144@gmail.com>
2025-09-08 22:16:43 -0400

    Added initial nextjs project

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/portfolio-site"
+};
+
+export default nextConfig;

56043b956cb22c3c50962bed1b7c68f81c23e3a9
qzali-dev <qzali144@gmail.com>
2025-09-08 23:36:01 -0400

    Try changing action file to see if it works with absolute paths.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/next.svg`}
+          src={`./next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/vercel.svg`}
+              src={`./vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/file.svg`}
+            src={`./file.svg`}
             alt="File icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/globe.svg`}
+            src={`./globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
